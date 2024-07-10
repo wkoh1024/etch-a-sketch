ae62ddf3780d72b78d6aed926247b8efe88bfb7e
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-07-10 13:32:11 -0400

    Changed to 16x16 and added hover color

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const mainContainer = document.querySelector("#main-container");
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 256; i++) {
     let addedDiv = document.createElement("div");
-    
+
     addedDiv.classList.add("pixels")
     mainContainer.append(addedDiv);
 }
